899b6bf4852ce3c14470437081823daabfc43035
pranab <pranab@ipvisionsoft.com>
2018-01-14 13:44:52 +0600

    added sieve implementation for getting all primes up to a specified number

---
+++ PrimeNumber/sieve.js
@@ -0,0 +1,31 @@
+console.log('----- All primes up to a specified number -----');
+
+/* Sieve method :: return a list of prime numbers up to a specified number */
+function sieveOfEratosthenes(n) {
+    var prime = Array(n).fill(true),
+        i,
+        j,
+        result = [];
+
+    for (i = 2; i <= 50; i++) {
+        if (prime[i]) {
+            result.push(i);
+
+            for (j = i * 2; j <= n; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    return result;
+}
+
+var listOfPrimeNumbers = sieveOfEratosthenes(50);
+console.log('All prime numbers up to 50');
+console.log(listOfPrimeNumbers);
+
+/* isPrime check from the list */
+var isPrime1 = listOfPrimeNumbers.indexOf(27), // false
+    isPrime2 = listOfPrimeNumbers.indexOf(29); // true
+
+console.log('27 is a prime number:', isPrime1 >= 0, '\n29 is a prime number:', isPrime2 >= 0);
