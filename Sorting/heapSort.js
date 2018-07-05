a986669b0d0bbf90b4a9093cb1a6bf1d35429301
Pranab <pranab.mitra@widespace.com>
2018-07-05 23:40:34 +0600

    Added Heap Sort

---
+++ Sorting/heapSort.js
@@ -0,0 +1,71 @@
+function getLeftChildIndex(index) {
+    return (2 * index) + 1;
+}
+
+function getRightChildIndex(index) {
+    return (2 * index) + 2;
+}
+
+function maxHeapify(arr, heapSize, index) {
+    let left,
+        right,
+        largest,
+        temp;
+
+    left = getLeftChildIndex(index);
+    right = getRightChildIndex(index);
+
+    if (left < heapSize && arr[left] > arr[index]) {
+        largest = left;
+    } else {
+        largest = index;
+    }
+
+    if (right < heapSize && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest !== index) {
+        temp = arr[index];
+        arr[index] = arr[largest];
+        arr[largest] = temp;
+
+        maxHeapify(arr, heapSize, largest);
+    }
+}
+
+function buildMaxHeap(arr, heapSize) {
+    let i;
+
+    for (i = Math.floor(heapSize / 2) - 1; i >= 0; i--) {
+        maxHeapify(arr, heapSize, i);
+    }
+}
+
+function heapSort(arr) {
+    let i,
+        temp,
+        heapSize = arr.length;
+
+    buildMaxHeap(arr, heapSize);
+
+    for (i = heapSize - 1; i >= 1; i--) {
+        temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapSize--;
+
+        maxHeapify(arr, heapSize, 0);
+    }
+}
+
+
+/*  Init  */
+var arr = [22, 30, 10, 7, 29, 23];
+
+console.log('Source: ', arr);
+
+heapSort(arr);
+console.log('After Heap Sort: ', arr);
+
