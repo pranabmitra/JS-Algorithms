7114db332db3b9da1b17fdc88d940e5e195d9f01
pranabmitra <pranab.cse04@gmail.com>
2020-04-05 02:29:14 +0900

    Two Sum

---
+++ problem-solving/LeetCode/two-sum/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let result = [];
+    const len = nums.length;
+    
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j < len; j++) {
+            if (nums[i] + nums[j] === target) {
+                result.push(i, j);
+            }
+        }
+    }
+    
+    return result;
+};
