a780d91c1bbf5ee090f9f3364b5ea4c2bd41c89c
pranabmitra <pranab.cse04@gmail.com>
2020-04-08 00:24:16 +0900

    Implement strStr()

---
+++ problem-solving/LeetCode/implement-strstr/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+
+/**
+ * Input: "hello", "ll"
+ * Output: 2
+ * 
+ * Input: "aaaaa", "bba"
+ * Output: -1
+ * 
+ * Input: "aaa", ""
+ * Output: 0
+ */
